d404100ed6095ad7fa36446712ed409514d54112
rladuddbs123 <rladuddbs789@gmail.com>
2020-12-07 14:58:05 +0900

    두 번째 세 번째 입력 값에 따라 데이터를 좌/우로 미는 기능 추가
    두 번째 입력값 음수인 경우 세 번째 입력값 변경
    두 번째 입력값에 따라 이동 횟수 설정
    세 번째 입력 값을 기준으로 하여 좌/우 이동 구현

--- script.js
+++ script.js
@@ -1,10 +1,63 @@
 var InputData = ''
 var DetachData = [];
+var text = [];
 function submit(value){    
     InputData = value;
     DetachData = InputData.split(' ');    
     errorCheck();
+    dataSet();
+    if(DetachData[2].toLowerCase() == 'r') {
+        pushRData()
+    } else {
+        pushLData()
+    }
 };
+
+function dataSet(){
+    // DetachData[0]에 입력 받은 문자를 1개씩 잘라서 배열에 넣음
+    text = DetachData[0].split('')
+    // DetachData[1]에 입력 받은 문자를 숫자로 변환 
+    DetachData[1] = parseInt(DetachData[1]);
+    
+    if(DetachData[1] < 0 ){
+        console.log("!23")            
+        if(DetachData[2].toLowerCase() == 'r'){
+            DetachData[2] = 'l';
+        } else {
+            DetachData[2] = 'r'
+        }
+        DetachData[1] = Math.abs(DetachData[1]);        
+    }    
+};
+
+function pushLData(){
+    for(var i = 0; i < DetachData[1]; i++){
+        var temporary = [];
+        for(var k = 0; k < text.length; k++){
+            if(k != text.length - 1){
+                    temporary[k] = text[k+1];
+                } else {
+                    temporary[k] = text[0];
+                };  
+            };
+        text = temporary;
+    }
+};
+
+function pushRData(){
+    for(var i = 0; i < DetachData[1]; i++){
+        var temporary = []
+            for(var a = 1; a < text.length+1; a++){
+                if(a != text.length){
+                    temporary[text.length-a] = text[text.length-a-1];
+                } else {
+                    temporary[0] = text[text.length-1];
+                }; 
+            }                 
+        text = temporary;
+    }
+};
+
 
 function errorCheck(){
     var error = document.getElementById("error");
