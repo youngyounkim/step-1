e561c1e2295412c96b33f1bf579368925eca811b
rladuddbs123 <rladuddbs789@gmail.com>
2020-12-07 12:38:57 +0900

    입력 받은 값이 정상인지 확인
    입력 받은 값이 3개가 아닌 경우 에러 발생
    입력 받은 값중 2번째 입력값이 숫자가 아닌 경우 에러 발생
    입력 받은 값중 3번째 입력값이 r,l,R,L이 아닌 경우 에러 발생

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-var inputData = ''
-
+var InputData = ''
+var DetachData = [];
 function submit(value){    
-    inputData = value
-    console.log(inputData)
+    InputData = value;
+    DetachData = InputData.split(' ');    
+    errorCheck();
+};
+
+function errorCheck(){
+    var error = document.getElementById("error");
+    if(DetachData.length != 3 || DetachData[2] === ""){                
+        error.innerHTML = "입력값을 잘못 입력하셨습니다.";
+        DetachData = [];
+    } else {
+        error.innerHTML = "";
+    };
+
+    if(isNaN(DetachData[1])){
+        error.innerHTML = "입력값을 잘못 입력하셨습니다.";
+        DetachData = [];
+    } else {
+        error.innerHTML = "";
+    };
+
+    if(DetachData[2] !== 'r' && DetachData[2] !== 'l' && DetachData[2] !== 'R' && DetachData[2] !== 'L'){
+        error.innerHTML = "입력값을 잘못 입력하셨습니다.";
+        DetachData = [];
+    } else{
+        error.innerHTML = "";
+    };
 }
